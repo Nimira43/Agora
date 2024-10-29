a1f19842f8ad3729fb77fbd552d65e5a35058936
Nimira43 <n.rabone72@aol.com>
2024-10-29 22:51:35 +0000

    Added custom button classes to index.css to override some of the Bootstrap button colours

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,15 +1,34 @@
-import { useParams } from 'react-router-dom' 
+import { Link, useParams } from 'react-router-dom' 
+import { 
+  Row, 
+  Col, 
+  Image, 
+  ListGroup, 
+  Card, 
+  Button 
+} from 'react-bootstrap'
+import Rating from '../components/Rating'
 import products from '../products'
 
 const ProductScreen = () => {
   const { id: productId } = useParams()
   const product = products.find((p) => p._id === productId)
-  console.log(product)
   
   return (
-    <div>
-      Product Screen
-    </div>
+    <>
+      <Link className='btn btn-dk my-3' to='/'>Return</Link>
+      <Row>
+        <Col md={5}>
+
+        </Col>
+        <Col md={4}>
+        
+        </Col>
+        <Col md={3}>        >
+
+        </Col>
+      </Row>
+    </>
   )
 }
 
