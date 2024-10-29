bbc7b92bdb3d28f356717e45d09e6500790f93f7
Nimira43 <n.rabone72@aol.com>
2024-10-29 21:49:42 +0000

    Used LinkContainer from React Router Bootstrap to route the logo, car and sign in links

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { BiShoppingBag } from 'react-icons/bi'
 import { LuUser2 } from 'react-icons/lu'
+import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../assets/logo1.png'
 
 const Header = () => {
@@ -8,15 +9,25 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            <img src={logo} alt='Agora Logo' width={25} className='logo'/>
-            Agora
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img src={logo} alt='Agora logo' width={25} className='logo'/>
+              Agora
             </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'><BiShoppingBag /> Cart</Nav.Link>
-              <Nav.Link href='/login'><LuUser2 /> Sign In</Nav.Link>
+              <LinkContainer to='/cart'>
+              <Nav.Link>
+                <BiShoppingBag /> Cart
+              </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+              <Nav.Link>
+                <LuUser2 /> Sign In
+              </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
