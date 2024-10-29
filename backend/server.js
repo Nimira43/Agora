13af2601e8ca7b694366a1d966c6cf1e0320e74f
Nimira43 <n.rabone72@aol.com>
2024-10-29 23:52:08 +0000

    Basic server running

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Server is alive!!!')
+import express from 'express'
+const port = 5001
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API running....')
+})
+
+app.listen(port, () => console.log(`Server running on Port ${port}`))
